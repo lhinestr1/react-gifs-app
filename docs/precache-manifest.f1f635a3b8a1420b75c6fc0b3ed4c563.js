2925cb7a56b4332e9bf28be53858032265388829
Luis Hinestroza <luishinestroza1992@gmail.com>
2020-06-29 02:41:38 -0500

    docs add

---
+++ docs/precache-manifest.f1f635a3b8a1420b75c6fc0b3ed4c563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "143e1c351fc8efdfae64f404da244da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "801524a09511d4d14985",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e710b8b8a74e7adf18c3",
+    "url": "/static/js/2.17b9a3e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17b9a3e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801524a09511d4d14985",
+    "url": "/static/js/main.0c8222e8.chunk.js"
+  },
+  {
+    "revision": "28c7eea5c09e41854f6a",
+    "url": "/static/js/runtime-main.50e90fe6.js"
+  }
+]);
