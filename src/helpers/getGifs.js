fd6dac8f2d903533f5cfcf11898bc4f61799212d
Luis Hinestroza <luishinestroza1992@gmail.com>
2020-06-29 02:55:04 -0500

    getGifs function updated

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -5,7 +5,7 @@ export const getGifs = async ( category='cars' ) => {
 
     try{
         const api_key = 'SLxKrY6Puqz2EkiTu48zkGyqqepsgoFT';
-        const resp = await fetch(`http://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=6`);
+        const resp = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${category}&limit=6`);
         const {data} = await resp.json();
         return data;
     }catch(error){
